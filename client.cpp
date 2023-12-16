a69cadbaf26bce5ba2b868eb2af55857bb741563
Fredy Goolberth Quispe Neira <fredy.quispe@ucsp.edu.pe>
2023-12-15 20:29:44 -0500

    client.cpp documented

--- client.cpp
+++ client.cpp
@@ -10,6 +10,7 @@ struct sockaddr_in server_addr;
 string SERVER_IP = "127.0.0.1";
 
 ACK_controller ack_controller;
+// In case of read response too long to be received
 map<string, vector<unsigned char>> incomplete_message; // msg_id, data
 int seq_number = 0;
 int msg_id = 0;
@@ -38,6 +39,7 @@ void send_packet_to_server(Packet packet);
 typedef void (*req_ptr)(stringstream&);
 typedef void (*recv_ptr)(vector<unsigned char>);
 
+// Map of user input allowed
 map<string, req_ptr> request_functions({
     {"create", &create_request},
     {"read", &read_request},
@@ -46,6 +48,7 @@ map<string, req_ptr> request_functions({
     {"delete", &delete_request}
 });
 
+// Map of packet type allowed
 map<string, recv_ptr> response_functions({
     {"R", &read_response},
     {"N", &recv_notification}
@@ -54,11 +57,13 @@ map<string, recv_ptr> response_functions({
 int main(){
     if ((serverFD = socket(AF_INET, SOCK_DGRAM, 0)) == -1)  ERROR("Socket")
 
+    // Define the server's address
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(5000);
     if (inet_pton(AF_INET, SERVER_IP.c_str(), &server_addr.sin_addr) == -1)   ERROR("inet_pton")
 
+    // Process user nickname, if it is too long, cut it into 9 characters
     cout << "Input your nickname: ";
     getline(cin, nickname);
     cin.clear();
@@ -100,6 +105,11 @@ void thread_receiver(){
     }   
 }
 
+/*
+    Decoding packet from server, if packet is not corrupted send one ACK, else send one more ACK
+    @param packet: Packet
+    @return void
+*/
 void decoding(Packet packet){
     
     string seq_num = packet.seq_num();
@@ -133,6 +143,11 @@ void decoding(Packet packet){
     }
 }
 
+/*
+    Encoding user input to send to server
+    @param buffer : user input
+    @return void
+*/
 void encoding(string buffer){
     stringstream ss(buffer);
     string action;
@@ -140,6 +155,7 @@ void encoding(string buffer){
     getline(ss, action, ' ');
     transform(action.begin(), action.end(), action.begin(), ::tolower);
 
+    // If action is not in request_functions
     if (request_functions.find(action) == request_functions.end()){
         cout << "Invalid action" << endl;
         return;
@@ -149,6 +165,12 @@ void encoding(string buffer){
 
 
 // CRUD request functions
+
+/*
+    Create request to the server, the relation node1->node2 is created and duplicated
+    @param ss : node1 node2
+    @return void
+*/
 void create_request(stringstream &ss){
     // ss : node1 node2
     Packet packet;
@@ -159,12 +181,18 @@ void create_request(stringstream &ss){
     getline(ss, node2, '\0');
     string size1 = format_int(node1.size(), 2);
     string size2 = format_int(node2.size(), 2);
+    // Format data : 00 node1 00 node2
     string data = size1 + node1 + size2 + node2;
     
     packet.set_data(data);
     send_packet_to_server(packet);
 }
 
+/*
+    Read request to the server, the relation node->node is read. depth = 1
+    @param ss : node
+    @return void
+*/
 void read_request(stringstream &ss){
     // ss : node
     Packet packet;
@@ -173,12 +201,18 @@ void read_request(stringstream &ss){
     string node;
     getline(ss, node, '\0');
     string size = format_int(node.size(), 2);
+    // Format data : 00 node 1
     string data = size + node + "1"; // Depth = 1
     
     packet.set_data(data);
     send_packet_to_server(packet);
 }
 
+/*
+    Recursive read request to the server, the relation node->nodes is read. depth = [1..3]
+    @param ss : depth node
+    @return void
+*/
 void rread_request(stringstream &ss){
     // ss : depth node
     Packet packet;
@@ -188,12 +222,18 @@ void rread_request(stringstream &ss){
     getline(ss, depth, ' ');
     getline(ss, node, '\0');
     string size = format_int(node.size(), 2);
+    // Format data : 00 node 0
     string data = size + node + depth;
     
     packet.set_data(data);
     send_packet_to_server(packet);
 }
 
+/*
+    Update request to the server, the relation node1->node2 is updated to node1->new2
+    @param ss : node1 node2 new2
+    @return void
+*/
 void update_request(stringstream &ss){
     // ss : node1 node2 new2
     Packet packet;
@@ -206,12 +246,18 @@ void update_request(stringstream &ss){
     string size1 = format_int(node1.size(), 2);
     string size2 = format_int(node2.size(), 2);
     string size3 = format_int(new2.size(), 2);
+    // Format data : 00 node1 00 node2 00 new2
     string data = size1 + node1 + size2 + node2 + size3 + new2;
     
     packet.set_data(data);
     send_packet_to_server(packet);
 }
 
+/*
+    Delete request to the server, the relation node1->node2 is deleted. If node2 = '*' all the relations with node1 are deleted
+    @param ss : node1 node2
+    @return void
+*/
 void delete_request(stringstream &ss){
     // ss : node1 node2
     Packet packet;
@@ -222,6 +268,7 @@ void delete_request(stringstream &ss){
     getline(ss, node2, '\0');
     string size1 = format_int(node1.size(), 2);
     string size2 = format_int(node2.size(), 2);
+    // Format data : 00 node1 00 node2
     string data = size1 + node1 + size2 + node2;
     
     packet.set_data(data);
@@ -230,6 +277,11 @@ void delete_request(stringstream &ss){
 
 // Receive functions
 
+/*
+    Process read response from the server, can be simple or recursive
+    @param data: formatted data (00node000node1,node2,etc. 00 = size of node, 000 = size of nodes)
+    @return void
+*/
 void read_response(stringstream &ss){
     // ss : 00node000node1,node2,etc
     string node_size(2, 0), nodes_size(3, 0);
@@ -246,6 +298,11 @@ void read_response(stringstream &ss){
     // TODO: Process the recursive read response
 }
 
+/*
+    Process notification from the server and show on screen
+    @param data: formatted data (00notification, 00 = size of data)
+    @return void
+*/
 void recv_notification(stringstream &ss){
     // ss : 00notification
     string size(2, 0);
@@ -255,8 +312,9 @@ void recv_notification(stringstream &ss){
     ss.read(notification.data(), notification.size());
     cout << "Notification received: " << notification << endl;
 }
+
 /*
-    Send packet to server
+    Send packet to server, all CRUD requests are sent without to be fragmented
     @param packet: packet to send with sections TYPE and DATA filled
     @return void
 */
@@ -267,36 +325,11 @@ void send_packet_to_server(Packet packet){
     packet.set_hash(calc_hash(packet.data()));
     packet.set_msg_id(format_int(msg_id, 3));
     packet.set_nickname(nickname);
-    packet.set_flag("0");
+    // Send message in one packet without fragmentation
+    packet.set_flag("0"); 
 
     send_packet(serverFD, server_addr, ack_controller, packet);
     
     seq_number = (seq_number + 1) % 100; // Increment sequence number
     msg_id = (msg_id + 1) % 1000; // Increment message id
-}
-
-void read_response(vector<unsigned char> data){
-    // data : 00node000node1,node2,etc
-    stringstream ss;
-    ss.write((char *)data.data(), data.size());
-    string node_size(2, 0), nodes_size(3, 0);
-
-    ss.read(node_size.data(), node_size.size());
-    string node(stoi(node_size), 0);
-    ss.read(node.data(), node.size());
-
-    ss.read(nodes_size.data(), nodes_size.size());
-    string nodes(stoi(nodes_size), 0);
-    cout << "Read response: " << node << "->" << nodes << endl;
-}
-
-void recv_notification(vector<unsigned char> data){
-    // data : 00notification
-    stringstream ss;
-    ss.write((char *)data.data(), data.size());
-    string size(2, 0);
-    ss.read(size.data(), size.size());
-    string notification(stoi(size), 0);
-    ss.read(notification.data(), notification.size());
-    cout << "Notification received: " << notification << endl;    
 }
